604ae9d5a23574486335b808adcc4897c75b3b7e
irwandyrosadi <irwandyrosadi@gmail.com>
2019-10-28 12:07:54 +0800

    membuat generik method runQuery untuk menjalankan query prepared statement

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 require 'DB.php';
-
 $DB = DB::getInstance();
 
+// $result = $DB->runQuery('SELECT * FROM barang WHERE id_barang = ?', [4]);
+// $tabelBarang = $result->fetchAll(PDO::FETCH_OBJ);
+
+// $query = "INSERT INTO barang (nama_barang, jumlah_barang, harga_barang) VALUES (?,?,?)";
+// $data  = ['Cosmos CRJ-8229 - Rice Cooker', 5, 299000];
+
+// $DB->runQuery($query, $data);
+
+$result = $DB->runQuery("SELECT * FROM barang");
+$tabelBarang = $result->fetchAll(PDO::FETCH_OBJ);
+
 echo "<pre>";
-var_dump($DB);
+print_r($tabelBarang);
 echo "</pre>";
